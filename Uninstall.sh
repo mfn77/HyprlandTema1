6c3085d0f3b6f4d647c82fce2fc3409790430143
MFN <124898723+mfn77@users.noreply.github.com>
2023-02-28 10:54:59 +0300

    Update Uninstall.sh

--- Uninstall.sh
+++ Uninstall.sh
@@ -4,7 +4,7 @@ CONFIG_DIR=$HOME/.config
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-cd $HOME
+cd "$HOME" || exit
 clear
 
 #Dependencies
@@ -17,36 +17,35 @@ yay -R --noconfirm 'hyprland-git' 'wlogout' 'rofi-emoji' 'dunst' 'polkit-gnome' 
   'kitty' 'qt5-wayland' 'qt6-wayland'\
   'apple-fonts' 'cmake' 'waybar-hyprland-git' 'cava' 'pavucontrol' 'viewnior' 'noise-suppression-for-voice' 'rustup'\
 BACK_PID=$!
-wait $BACK_PID
+wait "$BACK_PID"
 clear
 
-cd $HOME
+cd "$HOME" || exit
 
 #Removing Configs
 echo -e "\e[1;31m${bold}Konfigürasyon Kaldırılıyor${normal}\e[0m"
-rm -rf $HOME/.config/kitty
-rm -rf $HOME/.config/dunst
-rm -rf $HOME/.config/swaylock
-rm -rf $HOME/.config/nwg-look
-rm -rf $HOME/.config/viewnior
-rm -rf $HOME/.config/rofi
-rm -rf $HOME/.config/wlogout
-rm -rf $HOME/.config/hypr
-rm -rf $HOME/.config/waybar
-rm -rf $HOME/.config/xsettingsd
-rm -rf $HOME/.config/gtk-3.0
-rm -f $HOME/.config/mimeapps.list
-rm -rf $HOME/.scripts
-rm -rf $HOME/.wallpapers
-rm -rf $HOME/.local/bin
-rm -f $HOME/.autostart
-cp -Rv $DIR/.OldConfig/gtk-3.0 $CONFIG_DIR/
-cp -Rv $DIR/.OldConfig/xsettingsd $CONFIG_DIR/
-cp -v $DIR/.OldConfig/mimeapps.list $CONFIG_DIR/
+rm -rf "$HOME"/.config/kitty
+rm -rf "$HOME"/.config/dunst
+rm -rf "$HOME"/.config/swaylock
+rm -rf "$HOME"/.config/nwg-look
+rm -rf "$HOME"/.config/viewnior
+rm -rf "$HOME"/.config/rofi
+rm -rf "$HOME"/.config/wlogout
+rm -rf "$HOME"/.config/hypr
+rm -rf "$HOME"/.config/waybar
+rm -rf "$HOME"/.config/xsettingsd
+rm -rf "$HOME"/.config/gtk-3.0
+rm -f "$HOME"/.config/mimeapps.list
+rm -rf "$HOME"/.scripts
+rm -rf "$HOME"/.wallpapers
+rm -rf "$HOME"/.local/bin
+rm -f "$HOME"/.autostart
+cp -Rv "$DIR"/.OldConfig/gtk-3.0 "$CONFIG_DIR"/
+cp -Rv "$DIR"/.OldConfig/xsettingsd "$CONFIG_DIR"/
+cp -v "$DIR"/.OldConfig/mimeapps.list "$CONFIG_DIR"/
 clear
 
 #Removing Unneeded Dependencies And Upgrading the System
 yay -Yc --noconfirm
 yay -Syu --noconfirm
 clear
-
